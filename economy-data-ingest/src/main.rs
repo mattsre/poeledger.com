477fa5b6079c9c80cdd5d80f80ed4caf46add6c4
Matt <mattconway55@gmail.com>
2023-08-10 01:07:58 -0400

    feat: publish new economy-data crate, update ingest to process currency and items

--- economy-data-ingest/src/main.rs
+++ economy-data-ingest/src/main.rs
@@ -1,19 +1,11 @@
-use chrono::DateTime;
-use chrono::FixedOffset;
-use chrono::NaiveDate;
-use chrono::NaiveDateTime;
-use chrono::NaiveTime;
-use chrono::TimeZone;
-use chrono::Utc;
-use futures::prelude::*;
-use influxdb2::models::DataPoint;
-use influxdb2::Client;
-use poeledger_economy_data::CurrencyPriceRecord;
-use poeledger_economy_data::ItemPriceRecord;
-use poeledger_economy_data::PriceRecord;
 use std::env;
 use std::process::exit;
 
+use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Utc};
+use futures::prelude::*;
+use influxdb2::models::DataPoint;
+use influxdb2::Client;
+use poeledger_economy_data::{CurrencyPriceRecord, ItemPriceRecord, PriceRecord};
 use tokio::time::Instant;
 
 #[tokio::main]
@@ -35,8 +27,26 @@ async fn main() -> anyhow::Result<()> {
     let max_batch_size = 1000;
     let mut write_batch: Vec<DataPoint> = Vec::new();
 
-    for result in csv_reader.deserialize() {
-        let record: PriceRecord = result?;
+    let mut raw_record = csv::StringRecord::new();
+    let headers = csv_reader.headers()?.clone();
+
+    while csv_reader.read_record(&mut raw_record)? {
+        let record: PriceRecord = match raw_record.len() {
+            6 => {
+                let r: CurrencyPriceRecord = raw_record.deserialize(Some(&headers))?;
+                PriceRecord::CurrencyPriceRecord(r)
+            }
+            10 => {
+                let r: ItemPriceRecord = raw_record.deserialize(Some(&headers))?;
+                PriceRecord::ItemPriceRecord(r)
+            }
+            _ => {
+                eprintln!(
+                    "failed to determine whether this file contains currency or item price records"
+                );
+                exit(1);
+            }
+        };
 
         let datapoint = match record {
             PriceRecord::CurrencyPriceRecord(r) => match create_datapoint_from_cpr(r) {
@@ -97,35 +107,40 @@ async fn main() -> anyhow::Result<()> {
 fn create_datapoint_from_cpr(record: CurrencyPriceRecord) -> anyhow::Result<DataPoint> {
     let timestamp = get_nano_timestamp(record.date);
 
-    let datapoint = DataPoint::builder("currency_price_record")
+    let builder = DataPoint::builder("currency_price_record")
         .timestamp(timestamp)
         .tag("league", record.league.to_string())
         .tag("get", &record.get)
         .tag("pay", &record.pay)
         .tag("confidence", record.confidence.to_string())
-        .field("value", record.value)
-        .build()?;
+        .field("value", record.value);
 
-    Ok(datapoint)
+    Ok(builder.build()?)
 }
 
 fn create_datapoint_from_ipr(record: ItemPriceRecord) -> anyhow::Result<DataPoint> {
     let timestamp = get_nano_timestamp(record.date);
 
-    let datapoint = DataPoint::builder("item_price_record")
+    let mut builder = DataPoint::builder("item_price_record")
         .timestamp(timestamp)
         .tag("league", record.league.to_string())
         .tag("id", record.id.to_string())
         .tag("type", &record.item_type)
         .tag("name", &record.name)
-        .tag("baseType", &record.base_type.unwrap_or("".to_string()))
-        .tag("variant", &record.variant.unwrap_or("".to_string()))
-        .tag("links", &record.links.unwrap_or("".to_string()))
-        .tag("confidence", &record.confidence.to_string())
-        .field("value", record.value)
-        .build()?;
+        .tag("confidence", record.confidence.to_string())
+        .field("value", record.value);
 
-    Ok(datapoint)
+    if !&record.base_type.is_empty() {
+        builder = builder.tag("baseType", &record.base_type);
+    }
+    if !&record.variant.is_empty() {
+        builder = builder.tag("variant", &record.variant);
+    }
+    if !&record.links.is_empty() {
+        builder = builder.tag("links", &record.links);
+    }
+
+    Ok(builder.build()?)
 }
 
 fn get_nano_timestamp(date: NaiveDate) -> i64 {
@@ -136,4 +151,76 @@ fn get_nano_timestamp(date: NaiveDate) -> i64 {
     let dt_with_tz_utc: DateTime<Utc> = Utc.from_utc_datetime(&dt_with_tz.naive_utc());
 
     dt_with_tz_utc.timestamp_nanos()
+}
+
+#[cfg(test)]
+mod tests {
+    use poeledger_economy_data::{CurrencyPriceRecord, ItemPriceRecord, PriceRecord};
+    use serde::{Deserialize, Serialize};
+
+    #[test]
+    fn deserialize_currency_price_record() -> anyhow::Result<()> {
+        let data = "\
+League;Date;Get;Pay;Value;Confidence
+Sanctum;2022-12-09;Sacrifice at Dusk;Chaos Orb;1;High
+Sanctum;2022-12-10;Sacrifice at Dusk;Chaos Orb;0.9787;High
+Sanctum;2022-12-11;Sacrifice at Dusk;Chaos Orb;0.4618;High
+Sanctum;2022-12-12;Sacrifice at Dusk;Chaos Orb;0.27212;High
+Sanctum;2022-12-13;Sacrifice at Dusk;Chaos Orb;0.182;High
+";
+
+        let mut csv_reader = csv::ReaderBuilder::new()
+            .delimiter(b';')
+            .from_reader(data.as_bytes());
+
+        let mut raw_record = csv::StringRecord::new();
+        let headers = csv_reader.headers()?.clone();
+
+        let mut count = 0;
+        while csv_reader.read_record(&mut raw_record)? {
+            let _: CurrencyPriceRecord = raw_record
+                .deserialize(Some(&headers))
+                .expect("should deserialize to CurrencyPriceRecord");
+
+            count += 1;
+        }
+
+        let expected_deser_count = 5;
+        assert_eq!(count, expected_deser_count);
+
+        Ok(())
+    }
+
+    #[test]
+    fn deserialize_item_price_record() -> anyhow::Result<()> {
+        let data = "\
+League;Date;Id;Type;Name;BaseType;Variant;Links;Value;Confidence
+Sanctum;2022-12-09;2673;SkillGem;Dark Pact;;1/20;;1;Low
+Sanctum;2022-12-10;2673;SkillGem;Dark Pact;;1/20;;1.42;Medium
+Sanctum;2022-12-11;2673;SkillGem;Dark Pact;;1/20;;1.61;High
+Sanctum;2022-12-12;2673;SkillGem;Dark Pact;;1/20;;1.99;High
+Sanctum;2022-12-13;2673;SkillGem;Dark Pact;;1/20;;2;High
+";
+
+        let mut csv_reader = csv::ReaderBuilder::new()
+            .delimiter(b';')
+            .from_reader(data.as_bytes());
+
+        let mut raw_record = csv::StringRecord::new();
+        let headers = csv_reader.headers()?.clone();
+
+        let mut count = 0;
+        while csv_reader.read_record(&mut raw_record)? {
+            let _: ItemPriceRecord = raw_record
+                .deserialize(Some(&headers))
+                .expect("should deserialize to CurrencyPriceRecord");
+
+            count += 1;
+        }
+
+        let expected_deser_count = 5;
+        assert_eq!(count, expected_deser_count);
+
+        Ok(())
+    }
 }
